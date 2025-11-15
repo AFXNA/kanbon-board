a278f149f19c36d1c8f0130ed4d4c1a9d4337260
Naranjavkhlan Cassano Tumenbold <naranjavkhlan10@gmail.com>
2025-11-15 12:49:20 -0500

    Hello, World 2

---
+++ hahabruh/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './styles.css'
+
+export default function App() {
+  return(
+    <div>
+      <head>
+      <meta charset="UTF-8" />
+      <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>hahabruh</title>
+    </head>
+    <body>
+      <div id="root"></div>
+      <script type="module" src="/src/main.jsx"></script>
+    </body>
+    </div>
+  );
+}
