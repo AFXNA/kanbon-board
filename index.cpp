f15b2383c71727ca165f9233e798868853a80200
Naranjavkhlan Cassano Tumenbold <naranjavkhlan10@gmail.com>
2025-11-15 09:44:42 -0500

    Hello, World!

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    cout<<"Hello, World"<<endl;
+    
+
+    int i = 0; 
+    for(i = 0; i<=200; i++){
+        cout<<i*i<<endl;
+    }
+
+    return 12;
+}
